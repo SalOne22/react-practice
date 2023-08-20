7017465b108454fa5717ef34c726a619634f7d1b
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-08-20 14:03:31 +0200

    Added filter state

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { CiSearch } from 'react-icons/ci';
 import styles from './Filter.module.css';
+import { useDispatch } from 'react-redux';
+import { updateFilter } from '../../redux/filterSlice';
 
 export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const handleFilterUpdate = (evt) => {
+    const value = evt.target.value.toLowerCase();
+    dispatch(updateFilter(value));
+  };
+
   return (
     <div className={styles.search}>
       <div className={styles.searchWrapper}>
@@ -10,9 +19,10 @@ export const Filter = () => {
 
         <input
           className={styles.searchInput}
-          type='text'
-          id='search'
-          placeholder='Search something..'
+          type="text"
+          id="search"
+          placeholder="Search something.."
+          onChange={handleFilterUpdate}
         />
       </div>
     </div>
